9857a1808e4061c43371145603b46dce36303617
Genzer <genzers@gmail.com>
2020-07-16 11:44:54 +0700

    Increase version to 0.2.0

--- ivy_cms/__about__.py
+++ ivy_cms/__about__.py
@@ -1,6 +1,6 @@
 __all__ = ["name", "version", "description", "long_description"]
 
 name = "axonivy_cms_export"
-version = "0.1.0"
+version = "0.2.0"
 description = "Export CMS from an Axon.ivy Project"
 long_description=""
